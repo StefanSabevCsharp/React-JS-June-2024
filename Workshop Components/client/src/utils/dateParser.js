5abdbb310d1769893914d0fa37ec89fcd841e54d
Stefan Sabev <stevenbg91@gmail.com>
2024-07-07 17:15:49 +0200

    Add simple date parser to show the date more readable

---
+++ Workshop Components/client/src/utils/dateParser.js
@@ -0,0 +1,6 @@
+export default function formatDate(isoString) {
+    const date = new Date(isoString);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+}
+
