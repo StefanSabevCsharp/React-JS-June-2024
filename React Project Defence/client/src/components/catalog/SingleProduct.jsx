621457f4df56706766d7c1dca86fa1bb32f0d767
Stefan Sabev <stevenbg91@gmail.com>
2024-07-16 18:55:48 +0200

    Update SingleProduct.jsx

--- React Project Defence/client/src/components/catalog/SingleProduct.jsx
+++ React Project Defence/client/src/components/catalog/SingleProduct.jsx
@@ -1,70 +1,87 @@
-'use client'
+// 'use client'
 
-import { useState } from 'react'
-import { StarIcon } from '@heroicons/react/20/solid'
-import { Radio, RadioGroup } from '@headlessui/react'
+// import { useState } from 'react'
+// import { StarIcon } from '@heroicons/react/20/solid'
+// import { Radio, RadioGroup } from '@headlessui/react'
 
-const product = {
-    name: 'Basic Tee 6-Pack',
-    price: '$192',
-    href: '#',
-    breadcrumbs: [
-        { id: 1, name: 'Men', href: '#' },
-        { id: 2, name: 'Clothing', href: '#' },
-    ],
-    images: [
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-            alt: 'Two each of gray, white, and black shirts laying flat.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
-            alt: 'Model wearing plain black basic tee.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
-            alt: 'Model wearing plain gray basic tee.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
-            alt: 'Model wearing plain white basic tee.',
-        },
-    ],
-    colors: [
-        { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-        { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-        { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
-    ],
-    sizes: [
-        { name: 'XXS', inStock: false },
-        { name: 'XS', inStock: true },
-        { name: 'S', inStock: true },
-        { name: 'M', inStock: true },
-        { name: 'L', inStock: true },
-        { name: 'XL', inStock: true },
-        { name: '2XL', inStock: true },
-        { name: '3XL', inStock: true },
-    ],
-    description:
-        'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-    highlights: [
-        'Hand cut and sewn locally',
-        'Dyed with our proprietary colors',
-        'Pre-washed & pre-shrunk',
-        'Ultra-soft 100% cotton',
-    ],
-    details:
-        'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-}
-const reviews = { href: '#', average: 4, totalCount: 117 }
+// const product = {
+//     name: 'Basic Tee 6-Pack',
+//     price: '$192',
+//     href: '#',
+//     breadcrumbs: [
+//         { id: 1, name: 'Men', href: '#' },
+//         { id: 2, name: 'Clothing', href: '#' },
+//     ],
+//     images: [
+//         {
+//             src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
+//             alt: 'Two each of gray, white, and black shirts laying flat.',
+//         },
+//         {
+//             src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
+//             alt: 'Model wearing plain black basic tee.',
+//         },
+//         {
+//             src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
+//             alt: 'Model wearing plain gray basic tee.',
+//         },
+//         {
+//             src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
+//             alt: 'Model wearing plain white basic tee.',
+//         },
+//     ],
+//     colors: [
+//         { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+//         { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+//         { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+//     ],
+//     sizes: [
+//         { name: 'XXS', inStock: false },
+//         { name: 'XS', inStock: true },
+//         { name: 'S', inStock: true },
+//         { name: 'M', inStock: true },
+//         { name: 'L', inStock: true },
+//         { name: 'XL', inStock: true },
+//         { name: '2XL', inStock: true },
+//         { name: '3XL', inStock: true },
+//     ],
+//     description:
+//         'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+//     highlights: [
+//         'Hand cut and sewn locally',
+//         'Dyed with our proprietary colors',
+//         'Pre-washed & pre-shrunk',
+//         'Ultra-soft 100% cotton',
+//     ],
+//     details:
+//         'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+// }
+// const reviews = { href: '#', average: 4, totalCount: 117 }
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
+// function classNames(...classes) {
+//     return classes.filter(Boolean).join(' ')
+// }
+import { useState,useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../../dataService/requester";
+const BASE_URL = 'http://localhost:3030/jsonstore/clothes/clothes/';
 
 export default function SingleProduct() {
-    const [selectedColor, setSelectedColor] = useState(product.colors[0])
-    const [selectedSize, setSelectedSize] = useState(product.sizes[2])
+    const [product, setProduct] = useState({});
+    const productId = useParams()._id;
+    console.log(`id is ${productId}`);
+
+    useEffect(() => {
+        (
+            async () => {
+                const responce = await fetch(BASE_URL + productId);
+                
+                const data = await responce.json();
+                setProduct(data);
+            }
+        )();
+    }, []);
+    console.log(product);
 
     return (
         <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 mt-8">
@@ -76,16 +93,15 @@ export default function SingleProduct() {
                         {/* Image gallery */}
                         <div className="lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
                             <img
-                                alt={product.images[3].alt}
-                                src={product.images[3].src}
+                                src={product.imageUrl}
                                 className="h-full w-full object-cover object-center"
                             />
                         </div>
 
                         {/* Product info */}
                         <div className="px-4 pb-8 pt-8 sm:px-6 lg:pb-8 lg:pt-8">
-                            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
-                            <p className="text-3xl tracking-tight text-gray-900 mt-2">{product.price}</p>
+                            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.title}</h1>
+                            <p className="text-3xl tracking-tight text-gray-900 mt-2">${product.price}</p>
 
                             {/* Reviews */}
                            
