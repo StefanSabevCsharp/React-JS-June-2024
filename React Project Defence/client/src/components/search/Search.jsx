8ac1447b9831bb7c6210572254336c7624dd91f0
Stefan Sabev <stevenbg91@gmail.com>
2024-07-21 12:31:01 +0200

    Search using new custom hook

--- React Project Defence/client/src/components/search/Search.jsx
+++ React Project Defence/client/src/components/search/Search.jsx
@@ -1,52 +1,31 @@
-import Catalog from "../catalog/Catalog";
+
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import SearchInput from "./SearchInput";
 
 export default function Search() {
+ const products = useFetch("clothes/clothes", []).reverse();
+
     return (
         <>
-            <div className="relative isolate px-6 pt-14 lg:px-8 mt-10" >
-                <form className="max-w-md mx-auto">
-                    <label
-                        htmlFor="default-search"
-                        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-                    >
-                        Search
-                    </label>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg
-                                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            <SearchInput />
+            <div className="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-10">
+                    {products.map((product) => (
+                        <Link
+
+                            key={product._id} to={`/catalog/${product._id}`}className="group">
+                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                                <img
+                                    src={product.imageUrl}
+                                    className="h-full w-full object-cover object-center group-hover:opacity-75"
                                 />
-                            </svg>
-                        </div>
-                        <input
-                            type="search"
-                            id="default-search"
-                            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search ..."
-                            required=""
-                            style={{ color: "black" }}
-                        />
-                        <button
-                            type="submit"
-                            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        >
-                            Search
-                        </button>
-                    </div>
-                </form>
-            </div>
-            <Catalog />
+                            </div>
+                            <h3 className="mt-3 text-sm text-gray-700">{product.title}</h3>
+                            <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
+                        </Link>
+                    ))}
+                </div>
+
         </>
     );
 }
