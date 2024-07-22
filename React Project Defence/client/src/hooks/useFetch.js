946b805c21c0f59602386bcc46cb4595d3dbf691
Stefan Sabev <stevenbg91@gmail.com>
2024-07-22 18:49:02 +0200

    Updates of useFetch

--- React Project Defence/client/src/hooks/useFetch.js
+++ React Project Defence/client/src/hooks/useFetch.js
@@ -1,24 +1,32 @@
 import { useState, useEffect } from 'react';
-import { getUserData } from '../dataService/userData';
+
 
 const baseUrl = `http://localhost:3030/jsonstore`;
 
 function useFetch(url,initialState) {
 
     const [data, setData] = useState(initialState);
+    const [loading, setLoading] = useState(true);
+    const abortController = new AbortController();
 
     useEffect(() => {
+        setLoading(true);
         (
             async () => {
-                const responce = await fetch(`${baseUrl}/${url}`);
+                const responce = await fetch(`${baseUrl}/${url}` , {signal: abortController.signal});
                 const data = await responce.json();
                 setData(data);
+                setLoading(false);
             }
         )();
 
+        return () => {
+            abortController.abort();
+        }
+
     }, [url]);
 
-    return data;
+    return {data,loading};
 }
 
 export default useFetch;
