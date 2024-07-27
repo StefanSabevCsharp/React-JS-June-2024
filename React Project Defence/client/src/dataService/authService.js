d52102026b76328ce4a84c563d2d104ca457248d
Stefan Sabev <stevenbg91@gmail.com>
2024-07-27 15:56:18 +0200

    Add Auth service

---
+++ React Project Defence/client/src/dataService/authService.js
@@ -0,0 +1,16 @@
+import { post } from "./requester"
+
+
+const baseUrl = 'http://localhost:3030/users'
+
+export const login = async ({ email, password }) => {
+
+    const authData = await post(`${baseUrl}/login`, { email, password });
+
+    return authData;
+}
+
+export const register = async ( { email, password }) => {
+    const authData = await post(`${baseUrl}/register`, { email, password });
+    return authData;
+}
