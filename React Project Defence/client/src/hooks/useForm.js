8f829b7ebc3e2601155847efafed66539d2c5ad5
Stefan Sabev <stevenbg91@gmail.com>
2024-07-27 13:28:16 +0200

    Change use form
    Add submitHandler to the useForm hook

--- React Project Defence/client/src/hooks/useForm.js
+++ React Project Defence/client/src/hooks/useForm.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 
-function useForm(initialState) {
+function useForm(initialState,submitHandler) {
     const [form, setForm] = useState(initialState);
 
     const changeHandler = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
+    const onSubmit = (e) => {
+        e.preventDefault();
 
+        submitHandler(form);
+        setForm(initialState);
+    }
     return {
         form,
-        changeHandler
+        changeHandler,
+        onSubmit
     }
 }
 
