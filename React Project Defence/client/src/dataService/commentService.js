a975f3c4e9dd9a85e476fde4b200b077ce3de5fb
Stefan Sabev <stevenbg91@gmail.com>
2024-07-28 16:36:36 +0200

    Update commentService.js

--- React Project Defence/client/src/dataService/commentService.js
+++ React Project Defence/client/src/dataService/commentService.js
@@ -1,16 +1,20 @@
 import { get, post } from "./requester";
 
 
-const baseUrl = 'http://localhost:3030/jsonstore/clothes/clothes';
+const baseUrl = 'http://localhost:3030/data/comments';
 
-export async  function createComment({comment,productId}) {
+export async function createComment(comment, productId) {
 
-    return await post(`${baseUrl}/${productId}/comments`,{comment});
+    return await post(`${baseUrl}`, { comment, productId });
 
 
 }
 
 export async function getComments(productId) {
-    const data = await get(`${baseUrl}/${productId}/comments`);
-    return data;
+
+    const relations = encodeURIComponent('author=_ownerId:users');
+    const url = `${baseUrl}?where=productId%3D%22${productId}%22&sortBy=_createdOn%20desc&load=${relations}`;
+    console.log(url);
+    return await get(url);
+
 }
