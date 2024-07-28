e85079fa6b4c29602a336da304972381948ad19f
Stefan Sabev <stevenbg91@gmail.com>
2024-07-28 16:38:54 +0200

    Update clothesService.js

--- React Project Defence/client/src/dataService/clothesService.js
+++ React Project Defence/client/src/dataService/clothesService.js
@@ -1,10 +1,12 @@
 
-import { get, post, put } from "./requester";
+import { del, get, post, put } from "./requester";
 
 const baseUrl = 'http://localhost:3030/data/';
 
 export async function getAllClothes(){
-    const data = await  get(`${baseUrl}/clothes`);
+    const sortBy = '?sortBy=_createdOn desc';
+    const url = `${baseUrl}clothes${sortBy}`;
+    const data = await  get(url);
    
     return data;
 }
@@ -24,3 +26,22 @@ export async function updateClothes(id,data){
     const res = await put(`${baseUrl}clothes/${id}`,data);
     return res;
 }
+
+export function deleteClothes(id) { 
+    del(`${baseUrl}clothes/${id}`) 
+}
+
+export async function getLatestClothes(){
+    const sortBy = '?sortBy=_createdOn desc';
+    const pageSize = '&pageSize=4';
+    const url = `${baseUrl}clothes${sortBy}${pageSize}`;
+    
+    try {
+        const latestClothes = await get(url);
+        return latestClothes;
+    } catch (error) {
+        console.error('Error fetching latest clothes:', error);
+        throw error;
+    }
+}
+
