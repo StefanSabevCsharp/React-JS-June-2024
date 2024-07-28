c7c0016891671bcc424b33814c57f10f106a034e
Stefan Sabev <stevenbg91@gmail.com>
2024-07-28 16:38:39 +0200

    Changes in auth context

--- React Project Defence/client/src/context/authContext.jsx
+++ React Project Defence/client/src/context/authContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLogout } from "../hooks/useAuth";
-import { clearUserData } from "../dataService/userData";
+import { clearUserData, getUserData } from "../dataService/userData";
 
 
 const AuthContext = createContext();
@@ -11,12 +11,18 @@ export default AuthContext;
 export function AuthProvider(props){
     const navigate = useNavigate();
 
-    const [authState, setAuthState] = useState([]);
+    const [authState, setAuthState] = useState(() => {
+        const localdata = getUserData();
+        if(localdata){
+            return localdata;
+        }
+        return [];
+    });
 
     const contextData = {
         email: authState.email,
-        token: authState.accesToken,
-        userId: authState.userId,
+        accessToken: authState.accessToken,
+        userId: authState._id,
         isAuthenticated: authState.email,
         changeState: (state) => {
             setAuthState(state);
