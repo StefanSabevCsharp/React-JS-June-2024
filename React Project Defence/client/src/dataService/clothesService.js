431233c95b509110eaebb2bafb14d138ea9403af
Stefan Sabev <stevenbg91@gmail.com>
2024-07-28 12:07:27 +0200

    Add update cloth service

--- React Project Defence/client/src/dataService/clothesService.js
+++ React Project Defence/client/src/dataService/clothesService.js
@@ -1,19 +1,26 @@
-import useFetch from "../hooks/useFetch";
-import { get } from "./requester";
 
-const baseUrl = 'http://localhost:3030/jsonstore/clothes';
+import { get, post, put } from "./requester";
+
+const baseUrl = 'http://localhost:3030/data/';
 
 export async function getAllClothes(){
     const data = await  get(`${baseUrl}/clothes`);
+   
     return data;
 }
 
 export async function getSingleClothes(id){
-    const data = await get(`${baseUrl}/clothes/${id}`);
+    const data = await get(`${baseUrl}clothes/${id}`);
     return data;
 }
 
 export async function createClothes(data){
     const res = await post(`${baseUrl}/clothes`,data);
+    
+    return res;
+}
+
+export async function updateClothes(id,data){
+    const res = await put(`${baseUrl}clothes/${id}`,data);
     return res;
 }
