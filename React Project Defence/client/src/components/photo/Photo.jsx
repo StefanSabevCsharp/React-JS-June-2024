2236bfd7b7283d45f7d7a421a22fc8c93b64d1e9
Stefan Sabev <stevenbg91@gmail.com>
2024-07-30 18:24:53 +0200

    Add photo component

---
+++ React Project Defence/client/src/components/photo/Photo.jsx
@@ -0,0 +1,42 @@
+import { isImageUrl } from '../../../validations/formValidator';
+import useForm from '../../hooks/useForm';
+import styles from './style.module.css';
+
+export default function Photo({ closeModal , setPhoto }) {
+
+    const submitHandler = async (data) => {
+        const result = await isImageUrl(data.url);
+        if(!result){
+            alert('Invalid URL');
+            return;
+        }
+        setPhoto(data.url);
+        closeModal();
+       
+    }
+    const {form ,onSubmit , changeHandler} = useForm({url:''},submitHandler);
+    
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.modal} >
+                <button className={styles.closeButton} onClick={closeModal}>
+                    &times;
+                </button>
+                <h2>Enter URL</h2>
+                <form id="urlForm" onSubmit={onSubmit}>
+                    <label htmlFor="url">URL:</label>
+                    <input
+                        
+                        id="url"
+                        name="url"
+                        value={form.url}
+                        onChange={changeHandler}
+                        placeholder="https://example.com"
+                        required
+                    />
+                    <button type="submit" className={styles.submitButton}>Submit</button>        
+                </form>
+            </div>
+        </div>
+    );
+}
