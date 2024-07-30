a491d8ecc8a2999809356bf7f67a347c169a74f1
Stefan Sabev <stevenbg91@gmail.com>
2024-07-30 18:26:15 +0200

    Add Own listings component

---
+++ React Project Defence/client/src/components/ownListings/OwnListings.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+
+const people = [
+    {
+      name: 'Leslie Alexander',
+      email: 'leslie.alexander@example.com',
+      role: 'Co-Founder / CEO',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Michael Foster',
+      email: 'michael.foster@example.com',
+      role: 'Co-Founder / CTO',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Dries Vincent',
+      email: 'dries.vincent@example.com',
+      role: 'Business Relations',
+      imageUrl:
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: null,
+    },
+    {
+      name: 'Lindsay Walton',
+      email: 'lindsay.walton@example.com',
+      role: 'Front-end Developer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Courtney Henry',
+      email: 'courtney.henry@example.com',
+      role: 'Designer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    
+  ]
+  export default function OwnListings() {
+    return (
+      <div className="max-w-4xl mx-auto px-4">
+        <h1 className="text-2xl font-bold leading-tight text-gray-900 mb-8 text-center">My Listings</h1>
+        <ul role="list" className="grid grid-cols-2 gap-6">
+          {people.map((person) => (
+            <li key={person.email} className="flex items-center justify-center py-4">
+              <Link to={`/catalog/${person.id}`} className="flex flex-col items-center text-center">
+                <img alt={person.name} src={person.imageUrl} className="h-32 w-32 rounded-full bg-gray-50" />
+                <p className="mt-2 text-sm font-semibold leading-6 text-gray-900 truncate">{person.name}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
