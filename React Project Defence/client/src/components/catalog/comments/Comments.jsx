20b46228ba69ae93ceff8dd785da208fccc6a28d
Stefan Sabev <stevenbg91@gmail.com>
2024-07-30 18:25:50 +0200

    Update Comments.jsx

--- React Project Defence/client/src/components/catalog/comments/Comments.jsx
+++ React Project Defence/client/src/components/catalog/comments/Comments.jsx
@@ -1,23 +1,39 @@
-export default function Comments({comments}) {
+import { useEffect, useState } from "react";
+import splitName from "../../../utils/splitName";
 
-    
-    
+export default function Comments({comments}) {
+    const [loading,setLoading] = useState(true);
+
+    useEffect(() => {
+        if (comments) {
+            setLoading(false);
+        }
+    }, [comments]);
+
     
     return (
         <div className="bg-gray-100 p-4 max-w-2xl mx-auto">
             <h2 className="text-lg font-bold mb-4">Comments</h2>
             <div className="flex flex-col space-y-4">
-                {comments.length === 0 ? (
-                    <p className="text-gray-500">No comments yet.</p>
+                {loading ? (
+                    <div className="flex justify-center items-center">
+                        <div className="loader"></div>
+                    </div>
                 ) : (
-                    comments.map((comment, index) => (
-                        <div className="bg-white p-3 rounded-lg shadow-md" key={index}>
-                            <h3 className="text-lg font-bold">TO DO ADD USERNAME</h3>
-                            <p className="text-gray-700 text-sm">
-                               {comment.comment}
-                            </p>
-                        </div>
-                    ))
+                    comments?.length === 0 ? (
+                        <p className="text-gray-500">No comments yet.</p>
+                    ) : (
+                        comments?.map((comment, index) => (
+                            comment.author && comment.author.email ? (
+                                <div className="bg-white p-3 rounded-lg shadow-md" key={index}>
+                                    <h3 className="text-lg font-bold">{splitName(comment.author.email)}</h3>
+                                    <p className="text-gray-700 text-sm">
+                                       {comment.comment}
+                                    </p>
+                                </div>
+                            ) : null
+                        ))
+                    )
                 )}
             </div>
         </div>
