6f53a1ff2258c550759f705086cf2d2c26117a6b
Stefan Sabev <stevenbg91@gmail.com>
2024-07-30 18:24:16 +0200

    Add photo functionality

--- React Project Defence/client/src/components/profile/Profile.jsx
+++ React Project Defence/client/src/components/profile/Profile.jsx
@@ -1,10 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import OwnListings from "../ownListings/OwnListings";
-import AuthContext from "../../context/authContext";
+
 import splitName from "../../utils/splitName";
+import Photo from "../photo/Photo";
+import AuthContext from "../../context/authContext";
+
+
 
 export default function Profile() {
+   
+    const [click,setClick] = useState(false);
+    const [photoUrl,setPhotoUrl] = useState('');
     const ctx = useContext(AuthContext);
+
+    const handleClick = () => setClick(!click);
+    const closeModal = () => setClick(false);
+    const setPhotoHandler = (url) => {
+        setPhotoUrl(url);
+    };
+
     return (
         <>
             <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -20,23 +34,30 @@ export default function Profile() {
                             <div className="relative">
                                 {" "}
                                 <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-24 w-24"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        {" "}
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                                            clipRule="evenodd"
+                                {photoUrl ? (
+                                        <img
+                                            src={photoUrl}
+                                            alt="Profile"
+                                            className="w-full h-full object-cover rounded-full"
                                         />
-                                    </svg>{" "}
+                                    ) : (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-24 w-24 text-indigo-500"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    )}
                                 </div>{" "}
                             </div>{" "}
                             <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
-                                <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                                <button onClick={handleClick} className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
                                     {" "}
                                     Add Photo
                                 </button>{" "}
@@ -55,6 +76,8 @@ export default function Profile() {
                 </div>
             </div>
             <OwnListings />
+            {click && <Photo closeModal={closeModal} setPhoto={setPhotoHandler}/>}
+           
         </>
     );
 }
