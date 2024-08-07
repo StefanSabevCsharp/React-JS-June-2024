87b857a00849f6f054db15b60e352e7e56290eca
Stefan Sabev <stevenbg91@gmail.com>
2024-08-07 08:12:48 +0200

    Add tests for About component

--- React Project Defence/client/src/components/about/About.spec.jsx
+++ React Project Defence/client/src/components/about/About.spec.jsx
@@ -9,3 +9,28 @@ describe("Rendering the About component", () => {
         screen.debug();
     });
 });
+
+describe("Checking the content of the About component", () => {
+    it("should render the heading", () => {
+        render(<About/>);
+        const heading = screen.getByText(/About Us/i);
+        expect(heading).toBeInTheDocument();
+    });
+
+    it("should render the first paragraph", () => {
+        render(<About/>);
+        const firstParagraph = screen.getByText(/Our journey began with a simple idea/i);
+        expect(firstParagraph).toBeInTheDocument();
+    });
+
+    it("should render the second paragraph", () => {
+        render(<About/>);
+        const secondParagraph = screen.getByText(/Dedicated to excellence, we strive to exceed expectations/i);
+        expect(secondParagraph).toBeInTheDocument();
+    });
+    it("should render the image", () => {
+        render(<About/>);
+        const image = screen.getByAltText(/Company Image/i);
+        expect(image).toBeInTheDocument();
+    });
+});
